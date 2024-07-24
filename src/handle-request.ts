6b38d14f701676791f17cfafae977c9529547473
eggacheb <115781643+eggacheb@users.noreply.github.com>
2024-07-25 02:12:56 +0800

    Update handle-request.ts

--- src/handle-request.ts
+++ src/handle-request.ts
@@ -1,34 +1,7 @@
-const CORS_HEADERS: Record<string, string> = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "Content-Type, Authorization",
-};
-
-export default async function handleRequest(req: Request & { nextUrl?: URL }) {
-  if (req.method === "OPTIONS") {
-    return new Response(null, {
-      headers: CORS_HEADERS,
-    });
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    url.host = 'eggacheb-fast.hf.space';
+    return fetch(new Request(url, request));
   }
-
-  const { pathname, search } = req.nextUrl ? req.nextUrl : new URL(req.url);
-  // Update the base URL to point to the new target
-  const url = new URL(pathname + search, "https://eggacheb-fast.hf.space").href;
-  const headers = new Headers(req.headers); // Forward all request headers
-
-  const res = await fetch(url, {
-    body: req.body,
-    method: req.method,
-    headers,
-  });
-
-  const resHeaders = new Headers(res.headers);
-  for (const [key, value] of Object.entries(CORS_HEADERS)) {
-    resHeaders.set(key, value); // Add CORS headers to the response
-  }
-
-  return new Response(res.body, {
-    headers: resHeaders,
-    status: res.status
-  });
 }
