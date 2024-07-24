8ceb9ad0a083cbd3084a6a8dce796c1dee83ccb4
eggacheb <115781643+eggacheb@users.noreply.github.com>
2024-07-25 02:58:55 +0800

    Update handle-request.ts

--- src/handle-request.ts
+++ src/handle-request.ts
@@ -1,7 +1,31 @@
-export default {
-  async fetch(request: Request, env: any, ctx: ExecutionContext): Promise<Response> {
-    const url = new URL(request.url);
-    url.host = 'eggacheb-fast.hf.space';
-    return fetch(new Request(url.toString(), request));
+const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
+  const picked = new Headers();
+  for (const key of headers.keys()) {
+    if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
+      const value = headers.get(key);
+      if (typeof value === "string") {
+        picked.set(key, value);
+      }
+    }
   }
+  return picked;
 };
+
+export default async function handleRequest(req: Request & { nextUrl?: URL }) {
+  const { pathname, search } = req.nextUrl ? req.nextUrl : new URL(req.url);
+  const url = new URL(pathname + search, "https://eggacheb-fast.hf.space").href;
+  const headers = pickHeaders(req.headers, ["content-type", "authorization"]);
+
+  const res = await fetch(url, {
+    body: req.body,
+    method: req.method,
+    headers,
+  });
+
+  const resHeaders = Object.fromEntries(pickHeaders(res.headers, ["content-type", /^x-ratelimit-/, /^openai-/]));
+
+  return new Response(res.body, {
+    headers: resHeaders,
+    status: res.status,
+  });
+}
