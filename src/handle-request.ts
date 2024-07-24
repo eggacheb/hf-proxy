1ab3069b379c9e95462dd6b835f1fbabb6448c64
eggacheb <115781643+eggacheb@users.noreply.github.com>
2024-07-25 03:10:46 +0800

    Update handle-request.ts

--- src/handle-request.ts
+++ src/handle-request.ts
@@ -24,38 +24,30 @@ export default async function handleRequest(req: Request & { nextUrl?: URL }) {
     });
   }
 
-  if (req.method !== "GET") {
-    return new Response("Method Not Allowed", {
-      status: 405,
-      headers: CORS_HEADERS,
-    });
-  }
-
   const { pathname, search } = req.nextUrl ? req.nextUrl : new URL(req.url);
   const url = new URL(pathname + search, "https://eggacheb-fast.hf.space").href;
   const headers = pickHeaders(req.headers, ["content-type", "authorization"]);
 
-  try {
-    const res = await fetch(url, {
-      method: req.method,
-      headers,
-    });
+  const fetchOptions: RequestInit = {
+    method: req.method,
+    headers,
+  };
 
-    const resHeaders = {
-      ...CORS_HEADERS,
-      ...Object.fromEntries(
-        pickHeaders(res.headers, ["content-type", /^x-ratelimit-/, /^openai-/])
-      ),
-    };
-
-    return new Response(await res.text(), {
-      headers: resHeaders,
-      status: res.status,
-    });
-  } catch (error) {
-    return new Response("Error fetching the requested URL", {
-      status: 500,
-      headers: CORS_HEADERS,
-    });
+  if (req.method !== "GET" && req.method !== "HEAD") {
+    fetchOptions.body = req.body;
   }
+
+  const res = await fetch(url, fetchOptions);
+
+  const resHeaders = {
+    ...CORS_HEADERS,
+    ...Object.fromEntries(
+      pickHeaders(res.headers, ["content-type", /^x-ratelimit-/, /^openai-/])
+    ),
+  };
+
+  return new Response(res.body, {
+    headers: resHeaders,
+    status: res.status,
+  });
 }
