ba02b7c15460446a97e4ca57f2044d59b37aa480
eggacheb <115781643+eggacheb@users.noreply.github.com>
2024-07-25 02:14:28 +0800

    Update handle-request.ts

--- src/handle-request.ts
+++ src/handle-request.ts
@@ -1,5 +1,5 @@
 export default {
-  async fetch(request, env) {
+  async fetch(request: Request, env: any) {
     const url = new URL(request.url);
     url.host = 'eggacheb-fast.hf.space';
     return fetch(new Request(url, request));
